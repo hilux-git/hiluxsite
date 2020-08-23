7094956f66d32dae3975e0c3b5141f4b05015bcc
Author <27246275+hilux-git@users.noreply.github.com>
2020-08-23 01:02:52 -0700

    Update source.js

--- source.js
+++ source.js
@@ -2,6 +2,11 @@ function loadText() {
 window.open('file2.html');
 }
 
+
+
+
+
+
 var alphHigh = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     
 var alphLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -12,19 +17,42 @@ hiluxUno = alphHigh[0] + alphLow[3] + alphLow[12] + alphLow[8] + alphLow[13];
 hiluxDos = alphHigh[2] + alphLow[7] + alphLow[0] + alphLow[17] + alphHigh[15] + alphLow[0] + alphLow[18] + alphLow[18];
 
 
+
+
+
+
+
+
+
 function hiluxNo(){
 document.getElementById("no").innerHTML = "no";
 document.getElementById("yes").innerHTML = "";
         }
   
+
+
+
+
+
+
+
+
 function hiluxYes(){ 
 document.getElementById("yes").innerHTML = "correct"; 
 document.getElementById("no").innerHTML = "";
 document.getElementById("pass1").value = "";
-document.getElementById("pass2").value = "";
-document.getElementById("finalNo").innerHTML = "";
+document.getElementById("pass2").value = ""; 
 }    
   
+
+
+
+
+
+
+
+
+
 function hiluxEnter(){
 var hilux = document.getElementById('pass1').value;
 var hilux2 = document.getElementById('pass2').value;
@@ -45,7 +73,15 @@ var hilux2 = document.getElementById('pass2').value;
 }//function bracket
     
     
-       
+
+
+
+
+
+
+
+
+
     
 //hiluxtime   
 function hiluxTime() {
@@ -66,6 +102,14 @@ function checkTime(i) {
     
 
 
+
+
+
+
+
+
+
+
 function boxPop(){
 var boxDiv = document.getElementById("boxContainer");  
 
@@ -73,7 +117,16 @@ var boxDiv = document.getElementById("boxContainer");
         boxDiv.style.display = "block"
     }//boxDiv if statement bracket
 }
-    
+ 
+
+
+
+
+
+
+
+
+
 function hiluxFinalEntry(){
     var hilux3 = document.getElementById('pass3').value;
     var hilux4 = document.getElementById('pass4').value;  
@@ -85,19 +138,38 @@ function hiluxFinalEntry(){
     accessGranted();
 }
 }
-    if(hilux3 != buttonDiv.style.display + alphHigh[17] + alphLow[4] + alphHigh[11] + alphLow[14] + alphLow[6] && hilux4 != alphHigh[20] + alphLow[18] + alphLow[4] + alphLow[17] + alphHigh[11] + alphLow[14] + alphLow[6] + alphLow[8] + alphLow[13] + "!"){
+    if(hilux3 != buttonDiv.style.display + alphHigh[17] + alphLow[4] + alphHigh[11] + alphLow[14] + alphLow[6] || hilux4 != alphHigh[20] + alphLow[18] + alphLow[4] + alphLow[17] + alphHigh[11] + alphLow[14] + alphLow[6] + alphLow[8] + alphLow[13] + "!"){
     hiluxFinalNo();
     }
 }
-    
+ 
+
+
+
+
+
+
+
+
+
 function hiluxFinalNo(){
     document.getElementById('pass3').value = "";
     document.getElementById('pass4').value = "";
-    document.getElementById("finalNo").innerHTML = "no";
+    document.getElementById("no").innerHTML = "no";
     document.getElementById("yes").innerHTML = "";
     boxUnPop();
         } 
-    
+  
+
+
+
+
+
+
+
+
+
+
 function boxUnPop(){
 var boxDiv = document.getElementById("boxContainer");  
 
@@ -108,7 +180,17 @@ var boxDiv = document.getElementById("boxContainer");
     hiluxUno = hiluxUno + "1"
     hiluxDos = hiluxDos + "2"
 }
-    
+ 
+
+
+
+
+
+
+
+
+
+
 var hilux3Two;
 hilux3Two = alphHigh[17] + alphLow[4] + alphHigh[11] + alphLow[14] + alphLow[6];
 var hilux4Two;
