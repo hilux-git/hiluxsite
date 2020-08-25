c821387dc15941d15ac5478312a7d6ec0c8056bd
Author <27246275+hilux-git@users.noreply.github.com>
2020-08-24 18:44:56 -0700

    Update victor.js

--- victor.js
+++ victor.js
@@ -27,8 +27,21 @@ function theAccident(){
 
 function vicSpeak(){
 var userText = document.getElementById('vicAi').value;
-document.getElementById("vicAi").value = "";
-var numberHolder = Math.floor(Math.random() * 5);  
+if(userText.includes("do you") || userText.includes("will you")){
+    document.getElementById("vicsResponce").innerHTML = posNeg[Math.floor(Math.random() * posNeg.length)];
+    document.getElementById("vicAi").value = "";
+}else if(userText.includes('hello') && userText.includes("how are you") || userText.includes("how is your day")){
+    document.getElementById("vicsResponce").innerHTML = greetings[Math.floor(Math.random() * greetings.length)] + ", " + howAreYou[Math.floor(Math.random() * howAreYou.length)];
+    document.getElementById("vicAi").value = "";
+}else if(userText.includes('hi') && userText.includes("how are you") || userText.includes("how is your day")){
+    document.getElementById("vicsResponce").innerHTML = greetings[Math.floor(Math.random() * greetings.length)] + ", " + howAreYou[Math.floor(Math.random() * howAreYou.length)];
+    document.getElementById("vicAi").value = "";
+}else if(userText.includes("hello") || userText.includes("hi")){
+    document.getElementById("vicsResponce").innerHTML = greetings[Math.floor(Math.random() * greetings.length)];
+    document.getElementById("vicAi").value = "";
+}else{
+    document.getElementById("vicAi").value = "";
+    var numberHolder = Math.floor(Math.random() * 5);  
 if(numberHolder == 0){
     singleResponce();
 }else if(numberHolder == 1){
@@ -69,8 +82,19 @@ function eightResponce(){
 function tenResponce(){
     document.getElementById("vicsResponce").innerHTML = aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)] + " " + aiResponce[Math.floor(Math.random() * aiResponce.length)];
 }
+}
 
-      
+
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+
+var howAreYou = ["i am great", "i'm good", "im awesome", "great, how are you?", "today i'm having a great time", "i am doing amazing"]
+var greetings = ["hello", "hi", "greetings user", "greetings", "hello, how are you?", "what's up?", "hi!"];
+var posNeg = ["yes", "no", "absolutly", "i couldn't tell you", "no way!", "that's a good question", "i don't know", "sure"];   
 var aiResponce = ["yes", "no", "why", "I", "can", "do", "it", "my", "have", "a", "help", "will", "able", "to", "not", "speak", "user", "am", "the", "word", "you", "are", "go", "can't", "should", "thing", "with", "those", "they", "couldn't"];
     
     
