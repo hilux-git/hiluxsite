025e0ccc744aa1ecf63154ed8e78263105d196f5
Author <27246275+hilux-git@users.noreply.github.com>
2020-09-16 18:26:12 -0700

    Update lmpi.js

--- lmpi.js
+++ lmpi.js
@@ -14,6 +14,13 @@ function chClick(){
 window.open("partner.html", "_self"); 
 }
 
+function filler(){
+window.open("filler.html", "_self"); 
+}
+
+function herName(){
+window.open("hername.html", "_self"); 
+}
 
 
 function womanForward(){
@@ -24,3 +31,56 @@ var txt;
     
   } 
 }
+
+
+
+//WHY ARE YOU HERE LEAVE ME ALONG IGNORE THIS STOP. I WANT TO BE LEFT ALONE, THIS IS NOT ABOUT YOU IT's ABOUT VIC I WANT TO DO WHAT
+//I WANT TO DO YOU DONT HAVE TO WORRY ABOUT IT STOP ITS FOR ME NOT YOU. IF YOU ARE HERE WHY WHY DO YOU NEED TO WORRY I AM MOURNING
+//PLEASE PLEASE PLEASE please please please leave me alone i am fine!
+
+
+var arrayHerName = []
+function herNameActivate(){
+    var theNumber = Math.floor(Math.random() * 10) + 1;
+    arrayHerName.push(theNumber);
+    
+    document.getElementById("herName").innerHTML= "36459" + (arrayHerName.join(""));
+    setTimeout(herNameActivate, 5);
+}
+
+herNameActivate();
+
+
+//i feel its obvious his loss has changed me. i want to dig deeper and make everything a lot worse but ill keep it legal and clean i dont want to get in trouble, but my mind it hurts i miss vic. thank you
+
+function computerDecides(){
+var arrayValue = 30;
+var aiResponce = ["yes", "no", "why", "I", "can", "do", "it", "my", "have", "a", "help", "will", "able", "to", "not", "speak", "user", "am", "the", "word", "you", "are", "go", "can't", "should", "thing", "with", "those", "they", "couldn't", "smell"];
+    
+document.getElementById("contentHere").innerHTML= "<h3>" + aiResponce[Math.floor(Math.random() * arrayValue) + 1] + " " + aiResponce[Math.floor(Math.random() * arrayValue) + 1] + " " + aiResponce[Math.floor(Math.random() * arrayValue) + 1] + "?</h3>"    
+
+    
+var decider;
+var decider = Math.floor(Math.random() * 9) + 1;
+    
+    if(decider == 1){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://images.ctfassets.net/u0haasspfa6q/4jcW1pjS8UTfg7pfrsBhwx/c5a375fce6d8780e7ed0d7740cd2e4e8/cursed-images-3?w=320" onclick="herName()"></img>'
+}else if(decider == 2){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://pbs.twimg.com/media/CwJhMp1VUAAf2YS.jpg" onclick="herName()"></img>'
+}else if(decider == 3){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://pyxis.nymag.com/v1/imgs/392/b88/f2cae65a3354b4fc3a9ff4da05bd9821e7-31-birthday-boy.2x.h473.w710.jpg" onclick="herName()"></img>'
+}else if(decider == 4){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://www.meme-arsenal.com/memes/4e41d3092019ecbde94d4caf2df05a6b.jpg" onclick="herName()"></img>'
+}else if(decider == 5){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR-UJjriKmoSTuNq75SJG5ZjTYWTYdR224yxA&usqp=CAU" onclick="herName()"></img>'
+}else if(decider == 6){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_574/https://magazine.artland.com/wp-content/uploads/2020/03/Webp.net-compress-image-91.jpg" onclick="herName()"></img>'
+}else if(decider == 7){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSEoKNmOpW3CSBdT8leJHg_Qc6Xo533uWXRlA&usqp=CAU" onclick="herName()"></img>'
+}else if(decider == 8){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://static.wikia.nocookie.net/backrooms/images/9/99/3D9BF913-D75A-4BC6-AD71-FE439797A81E.jpeg/revision/latest/scale-to-width-down/340?cb=20200531221248" onclick="herName()"></img>'
+}else if(decider == 9){
+    document.getElementById("contentHere2").innerHTML = '<img src="https://i.ytimg.com/vi/VqZEfqoTA7M/maxresdefault.jpg" onclick="herName()"></img>'
+}}
+
+//i cut my finger off. no no noono i didnt no i didnt stop i did not, its right here
